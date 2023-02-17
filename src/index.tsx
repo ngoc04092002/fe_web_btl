2c651d4dd4e65af42c8d886a8f6b89241f53d35f
ngoc04092002 <vungoc23387@gmail.com>
2023-02-17 21:53:23 +0700

    new commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 // eslint-disable-next-line import/order
 import ReactDOM from 'react-dom/client';
 
-import '../styles/globals.css';
+import './styles/globals.css';
 import { Provider } from 'react-redux';
 
-import App from './App';
-import { store } from '../store/store';
+import App from './pages/App';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
