b25a8ce7120c8a4bcf7495c19038938515fd4192
ngoc04092002 <vungoc23387@gmail.com>
2023-04-24 07:29:36 +0700

    building UI create news

--- src/components/QuillText/QuillText.tsx
+++ src/components/QuillText/QuillText.tsx
@@ -5,9 +5,10 @@ import {
 	MenuItem,
 	Select,
 	SelectChangeEvent,
+	TextField,
 } from '@mui/material';
 import classNames from 'classnames/bind';
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import ReactQuill from 'react-quill';
 
 import ButtonWrapper from '../helpers/ButtonWrapper/ButtonWrapper';
@@ -30,14 +31,22 @@ const modules = {
 	],
 };
 
+const sliceSrcImgRegex: RegExp = /src\s*=\s*"(.+?)"/gim;
+
 const QuillText: FC<Props> = () => {
 	const [value, setValue] = useState('');
 	const [topic, setTopic] = useState('');
 	const [type, setType] = useState('');
 	const [topicSeleted, setTopicSelected] = useState<ITopicNewsData | null>(null);
 	const [typeSeleted, setTypeSelected] = useState<{ to: string; type: string } | null>(null);
+	// const [listNewsPiece, setListNewsPiece] = useState(null);
 
 	const [img, setImg] = useState<{ url: string; file: File | null }>({
+		url: '',
+		file: null,
+	});
+
+	const [imgMain, setImgMain] = useState<{ url: string; file: File | null }>({
 		url: '',
 		file: null,
 	});
@@ -53,7 +62,16 @@ const QuillText: FC<Props> = () => {
 		setTopicSelected(value);
 	};
 
-	const handlePreviewImg = (e: React.ChangeEvent<HTMLInputElement>) => {
+	const handlePreviewImgMain = (e: React.ChangeEvent<HTMLInputElement>) => {
+		if (!e.target.files?.length) {
+			return;
+		}
+
+		const file: string = URL.createObjectURL(e.target.files[0]);
+		setImgMain({ url: file, file: e.target.files[0] as unknown as File });
+	};
+
+	const handlePreviewImgNewsPiece = (e: React.ChangeEvent<HTMLInputElement>) => {
 		if (!e.target.files?.length) {
 			return;
 		}
@@ -71,11 +89,49 @@ const QuillText: FC<Props> = () => {
 		console.log('record');
 	};
 	console.log(value, img, topic, typeSeleted);
+	console.log('regex=>>', value.replace(sliceSrcImgRegex, "src='ok'"));
+
+	useEffect(() => {
+		return () => {
+			img && URL.revokeObjectURL(img.url);
+		};
+	}, [img]);
+
 	return (
 		<div className={`${cx('quill_text-container')} mt-8 mb-20`}>
 			<div className='mb-4 bg-white w-full p-4'>
-				<h1 className='text-xl font-bold mb-4'>Tạo tin tức</h1>
+				<div className='flex items-center justify-between'>
+					<h1 className='text-xl font-bold mb-4'>Tạo tin tức</h1>
+					<Button
+						variant='contained'
+						component='label'
+						className='text-center mb-2 mt-8'
+					>
+						Thêm ảnh
+						<input
+							hidden
+							accept='image/*'
+							multiple
+							type='file'
+							onChange={handlePreviewImgMain}
+						/>
+					</Button>
+				</div>
 				<div>
+					<TextField
+						id='title'
+						label='TItle'
+						variant='outlined'
+						className='w-full mb-2'
+						multiline
+					/>
+					<TextField
+						id='des'
+						label='Des'
+						variant='outlined'
+						className='w-full mb-2'
+						multiline
+					/>
 					<FormControl
 						fullWidth
 						className='mb-2'
@@ -127,22 +183,30 @@ const QuillText: FC<Props> = () => {
 							</Select>
 						</FormControl>
 					)}
+					<img
+						src={imgMain.url}
+						alt=''
+						className='h-[300px]'
+					/>
 				</div>
-				<div className='w-full'>
-					<Button
-						variant='contained'
-						component='label'
-						className='text-center mb-2 mt-8'
-					>
-						Thêm ảnh
-						<input
-							hidden
-							accept='image/*'
-							multiple
-							type='file'
-							onChange={handlePreviewImg}
-						/>
-					</Button>
+				<div className='w-full mt-8'>
+					<div className='flex items-center justify-between'>
+						<h1 className='text-xl font-bold mb-4'>Tạo các ý nhỏ</h1>
+						<Button
+							variant='contained'
+							component='label'
+							className='text-center mb-2 mt-2'
+						>
+							Thêm ảnh
+							<input
+								hidden
+								accept='image/*'
+								multiple
+								type='file'
+								onChange={handlePreviewImgNewsPiece}
+							/>
+						</Button>
+					</div>
 					<ReactQuill
 						className='bg-white'
 						theme='snow'
